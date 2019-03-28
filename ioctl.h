f35d2f548da2166295ffcffeed16e388161f543f
chen8958 <tony8958@gmail.com>
2019-03-28 16:55:53 +0800

    add ioctl.h(unfinish)

---
+++ ioctl.h
@@ -0,0 +1,41 @@
+typedef struct _7Seg_Info{
+  unsigned char Mode;
+  unsigned char Which;
+  unsigned long Value;
+} _7seg_info_t;
+
+#ifndef _7SEG_MODE_PATTERN
+#define _7SEG_MODE_PATTERN 0
+#endif
+
+#ifndef  _7SEG_MODE_HEX_VALUE
+#define _7SEG_MODE_HEX_VALUE 1
+#endif
+
+#ifndef  _7SEG_D5_INDEX
+#define _7SEG_D5_INDEX 8
+#endif
+
+#ifndef  _7SEG_D6_INDEX
+#define _7SEG_D6_INDEX 4
+#endif
+
+#ifndef  _7SEG_D7_INDEX
+#define _7SEG_D7_INDEX 2
+#endif
+
+#ifndef  _7SEG_D8_INDEX
+#define _7SEG_D8_INDEX 1
+#endif
+
+#ifndef  _7SEG_ALL
+#define _7SEG_ALL (_7SEG_D5_INDEX|_7SEG_D6_INDEX|_7SEG_D7_INDEX|_7SEG_D8_INDEX)
+#endif
+
+_7seg_info_t  data;
+int           fd, ret, i;
+if ((fd = open('/dev/lcd',O_RDWR)) <0 ) {
+  return (-1);
+  /* code */
+}
+ioctl(fd, _7SEG_IOCTL_ON, NULL);
